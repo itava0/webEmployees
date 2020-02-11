3ff4c41745869a572e6106dbdd96a0a1cc86eab0
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-11 15:53:25 -0500

    Created two methods to help me filter the employees

--- src/main/java/com/lambdaschool/webemployees/EmpList.java
+++ src/main/java/com/lambdaschool/webemployees/EmpList.java
@@ -11,4 +11,26 @@ public class EmpList {
         empList.add(new Employee("Sam", "Ford", 80000, true, 1, 1));
         empList.add(new Employee("John", "Jones", 75000, true, 1, 1));
     }
+
+    //Method to find an employee
+    public Employee findEmployee(CheckEmployee tester) {
+        for (Employee e : empList) {
+            if(tester.test(e)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Employee> findEmployees(CheckEmployee tester) {
+
+        ArrayList<Employee> temEmpList = new ArrayList<>();
+
+        for (Employee e: empList) {
+            if (tester.test(e)) {
+                temEmpList.add(e);
+            }
+        }
+        return temEmpList;
+    }
 }
